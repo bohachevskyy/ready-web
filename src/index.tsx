ba6463001b9295aad498972e268b0bebccd4bfdb
bohachevskyy <dmytro.bohachevskyy@gmail.com>
2026-02-22 19:45:27 -0800

    feat: integrate PostHog analytics (STO-53)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { PostHogProvider } from '@posthog/react';
 import * as Sentry from '@sentry/react';
 import { store, persistor } from './store/store';
 import './styles/index.css';
@@ -12,14 +13,21 @@ import { initializeSentry } from './config/sentry';
 // Initialize Sentry BEFORE React root creation
 initializeSentry();
 
+const posthogOptions = {
+  api_host: process.env.REACT_APP_POSTHOG_HOST,
+  defaults: '2026-01-30',
+} as const;
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <PostHogProvider apiKey={process.env.REACT_APP_POSTHOG_KEY!} options={posthogOptions}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </PostHogProvider>
   </React.StrictMode>
 );
 
